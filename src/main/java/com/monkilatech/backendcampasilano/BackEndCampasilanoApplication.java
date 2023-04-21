515c3dc80402deab33bf3218b16ef774511d9105
Joe Monkila <jnkiwa25@gmail.com>
2023-04-21 09:23:52 +0100

    init commit

---
+++ src/main/java/com/monkilatech/backendcampasilano/BackEndCampasilanoApplication.java
@@ -0,0 +1,13 @@
+package com.monkilatech.backendcampasilano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndCampasilanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndCampasilanoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/monkilatech/backendcampasilano/BackEndCampasilanoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monkilatech.backendcampasilano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndCampasilanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
