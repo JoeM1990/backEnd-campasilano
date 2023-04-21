442c3c459a4e1b9cec7e64df194da8debc0a5e02
Joe Monkila <jnkiwa25@gmail.com>
2023-04-21 09:34:48 +0100

    agents models

---
+++ src/main/java/com/monkilatech/backendcampasilano/model/agents.java
@@ -0,0 +1,26 @@
+package com.monkilatech.backendcampasilano.model;
+
+public class agents {
+
+    int id;
+    String uid;
+    String matricule;
+    String name;
+    String firstname;
+    int genre;
+    String phone;
+    String email;
+    String username;
+    String password;
+    String roles;
+    boolean status;
+    String locationId;
+    int fonctionId;
+    String fonction;
+    int promotionId;
+    String promotion;
+    String etablissementId;
+    String street;
+    String commonId;
+    String photo;
+}
