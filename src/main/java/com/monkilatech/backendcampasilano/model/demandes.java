fdf3bc9d3299cc627992aa14c86a7c650e4a8880
Joe Monkila <jnkiwa25@gmail.com>
2023-04-21 11:36:50 +0100

    commandes model

---
+++ src/main/java/com/monkilatech/backendcampasilano/model/demandes.java
@@ -0,0 +1,46 @@
+package com.monkilatech.backendcampasilano.model;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class demandes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+
+    @JsonInclude(Include.NON_EMPTY)
+    String fromUid;
+
+    @JsonInclude(Include.NON_EMPTY)
+    String senderUid;
+
+    @JsonInclude(Include.NON_EMPTY)
+    String sender;
+
+    @JsonInclude(Include.NON_EMPTY)
+    String photo;
+
+    @JsonInclude(Include.NON_EMPTY)
+    String promotion;
+
+    @JsonInclude(Include.NON_EMPTY)
+    String content;
+
+    @JsonInclude(Include.NON_EMPTY)
+    Date dateCreation;
+}
