434337602447eb2a17042bfb798ceccafae056b7
Joe Monkila <jnkiwa25@gmail.com>
2023-04-21 11:23:56 +0100

    etablissement model

---
+++ src/main/java/com/monkilatech/backendcampasilano/model/etablissement.java
@@ -0,0 +1,43 @@
+package com.monkilatech.backendcampasilano.model;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class etablissement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+
+    @JsonInclude(Include.NON_EMPTY)
+    String sigleName;
+
+    @JsonInclude(Include.NON_EMPTY)
+    String name;
+
+    @JsonInclude(Include.NON_EMPTY)
+    String logo;
+
+    @JsonInclude(Include.NON_EMPTY)
+    boolean status;
+
+    @JsonInclude(Include.NON_EMPTY)
+    Date dateCreation;
+
+    @JsonInclude(Include.NON_EMPTY)
+    int access;
+}
