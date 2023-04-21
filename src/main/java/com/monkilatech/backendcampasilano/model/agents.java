cb65ee486b1fcccee4c9cc18b769ecea7939d68c
Joe Monkila <jnkiwa25@gmail.com>
2023-04-21 16:12:30 +0100

    update all

--- src/main/java/com/monkilatech/backendcampasilano/model/agents.java
+++ src/main/java/com/monkilatech/backendcampasilano/model/agents.java
@@ -1,12 +1,7 @@
 package com.monkilatech.backendcampasilano.model;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import javax.persistence.*;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,63 +16,43 @@ public class Agents {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     int id;
 
-    @JsonInclude(Include.NON_EMPTY)
     String uid;
 
-    @JsonInclude(Include.NON_EMPTY)
     String matricule;
 
-    @JsonInclude(Include.NON_EMPTY)
     String name;
 
-    @JsonInclude(Include.NON_EMPTY)
     String firstname;
 
-    @JsonInclude(Include.NON_EMPTY)
     int genre;
 
-    @JsonInclude(Include.NON_EMPTY)
     String phone;
 
-    @JsonInclude(Include.NON_EMPTY)
     String email;
 
-    @JsonInclude(Include.NON_EMPTY)
     String username;
 
-    @JsonInclude(Include.NON_EMPTY)
     String password;
 
-    @JsonInclude(Include.NON_EMPTY)
     String roles;
 
-    @JsonInclude(Include.NON_EMPTY)
     boolean status;
 
-    @JsonInclude(Include.NON_EMPTY)
     String locationId;
 
-    @JsonInclude(Include.NON_EMPTY)
     int fonctionId;
 
-    @JsonInclude(Include.NON_EMPTY)
     String fonction;
 
-    @JsonInclude(Include.NON_EMPTY)
     int promotionId;
 
-    @JsonInclude(Include.NON_EMPTY)
     String promotion;
 
-    @JsonInclude(Include.NON_EMPTY)
     String etablissementId;
 
-    @JsonInclude(Include.NON_EMPTY)
     String street;
 
-    @JsonInclude(Include.NON_EMPTY)
     String commonId;
 
-    @JsonInclude(Include.NON_EMPTY)
     String photo;
 }
--- src/main/java/com/monkilatech/backendcampasilano/model/user.java
+++ src/main/java/com/monkilatech/backendcampasilano/model/user.java
@@ -5,10 +5,7 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import javax.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
