d9860d1066aaa02f9488f45b2aed59e93d22f2fb
Joe Monkila <jnkiwa25@gmail.com>
2023-04-21 11:33:55 +0100

    events model

---
+++ src/main/java/com/monkilatech/backendcampasilano/model/events.java
@@ -0,0 +1,59 @@
+package com.monkilatech.backendcampasilano.model;
+
+import java.util.Date;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class events {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+
+    @JsonInclude(Include.NON_EMPTY)
+    String name;
+
+    @JsonInclude(Include.NON_EMPTY)
+    String description;
+
+    @JsonInclude(Include.NON_EMPTY)
+    String content;
+
+    @JsonInclude(Include.NON_EMPTY)
+    String cover;
+
+    @JsonInclude(Include.NON_EMPTY)
+    Date dateOfEvent;
+
+    @JsonInclude(Include.NON_EMPTY)
+    boolean isPublic;
+
+    @JsonInclude(Include.NON_EMPTY)
+    String etablissementId;
+
+    @JsonInclude(Include.NON_EMPTY)
+    String etablissement;
+
+    @JsonInclude(Include.NON_EMPTY)
+    List<String> participants;
+
+    @JsonInclude(Include.NON_EMPTY)
+    int counts;
+
+    @JsonInclude(Include.NON_EMPTY)
+    boolean status;
+}
