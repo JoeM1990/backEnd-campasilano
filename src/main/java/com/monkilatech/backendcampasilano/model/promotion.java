3ee692e9dfd48139459dd7285364373e35dca51f
Joe Monkila <jnkiwa25@gmail.com>
2023-04-21 11:31:16 +0100

    promotion model

---
+++ src/main/java/com/monkilatech/backendcampasilano/model/promotion.java
@@ -0,0 +1,40 @@
+package com.monkilatech.backendcampasilano.model;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class promotion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+
+    @JsonInclude(Include.NON_EMPTY)
+    String name;
+
+    @JsonInclude(Include.NON_EMPTY)
+    int validation;
+
+    @JsonInclude(Include.NON_EMPTY)
+    boolean status;
+
+    @JsonInclude(Include.NON_EMPTY)
+    int etablissementId;
+
+    @JsonInclude(Include.NON_EMPTY)
+    Date dateCreation;
+}
