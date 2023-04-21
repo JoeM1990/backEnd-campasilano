aa9429653013dbe850ef82bb5a9fb34c948d6077
Joe Monkila <jnkiwa25@gmail.com>
2023-04-21 13:17:20 +0100

    agents controller

--- src/main/java/com/monkilatech/backendcampasilano/controller/AgentsController.java
+++ src/main/java/com/monkilatech/backendcampasilano/controller/AgentsController.java
@@ -1,8 +1,11 @@
 package com.monkilatech.backendcampasilano.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,6 +42,21 @@ public class AgentsController {
             e.printStackTrace();
         }
         return ResponseEntity.status(httpStatus).body(statusResponse);
+    }
+
+    @GetMapping
+    public ResponseEntity getAll() {
+        StatusResponse statusResponse = new StatusResponse();
+        try {
+            List<Agents> schools = this.agentService.getAll();
+
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(schools);
+        } catch (Exception e) {
+            statusResponse.setStatus("Erreur interne");
+            e.printStackTrace();
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(statusResponse);
     }
 
 }
