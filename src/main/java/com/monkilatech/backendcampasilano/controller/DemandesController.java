43718e15cfaa9df147e3a530956d3dd2486c63eb
Joe Monkila <jnkiwa25@gmail.com>
2023-04-24 16:54:48 +0100

    .

--- src/main/java/com/monkilatech/backendcampasilano/controller/DemandesController.java
+++ src/main/java/com/monkilatech/backendcampasilano/controller/DemandesController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.monkilatech.backendcampasilano.model.Demandes;
@@ -18,6 +19,7 @@ import com.monkilatech.backendcampasilano.utils.StatusResponse;
 import com.monkilatech.backendcampasilano.utils.ValueException;
 
 @RestController
+@RequestMapping("/api")
 public class DemandesController {
 
     @Autowired
