517b3e7ead04297c2e1d6776771ddbad21894453
Joe Monkila <70669743+JoeM1990@users.noreply.github.com>
2024-06-20 09:22:25 +0100

    Update StatusResponse.java

--- src/main/java/com/monkilatech/backendcampasilano/utils/StatusResponse.java
+++ src/main/java/com/monkilatech/backendcampasilano/utils/StatusResponse.java
@@ -6,10 +6,12 @@ import lombok.Data;
 
 @Data
 public class StatusResponse<T> {
+    
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
     private String status;
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
     private String message;
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
     private Object data;
+    
 }
