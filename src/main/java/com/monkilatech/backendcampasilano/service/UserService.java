68d00d02e4ac439bbaaaf92494fbfcd1285b9acb
Joe Monkila <70669743+JoeM1990@users.noreply.github.com>
2024-06-20 09:25:25 +0100

    Update UserService.java

--- src/main/java/com/monkilatech/backendcampasilano/service/UserService.java
+++ src/main/java/com/monkilatech/backendcampasilano/service/UserService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.monkilatech.backendcampasilano.model.User;
 
 public interface UserService {
+    
     public List<User> getAll() throws Exception;
 
     public User get(long userId) throws Exception;
@@ -14,4 +15,5 @@ public interface UserService {
     public User update(User user) throws Exception;
 
     public boolean delete(long userId) throws Exception;
+    
 }
